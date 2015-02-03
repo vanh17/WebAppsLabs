322d525d8dd1d53652f3004bd139f0fc862d299a
vanh17 <vanh17@hanover.edu>
2015-02-03 14:36:18 +0700

    "Final version of lab1."

--- yourCode.js
+++ yourCode.js
@@ -7,27 +7,43 @@
  */
 var binarySearch = function binarySearch(arr, val) {
    var lo, hi, mid;
-
-   // You may need to add things here
-
-   while (false) {     // You should change this with a proper condition
-      // You will need to add things here
+   if (arr.length === 0)
+   {
+      return false;
    }
-
+   lo = 0;
+   hi = arr.length - 1;
    // You may need to add things here
-
+   if (arr[lo] > val || arr[hi] < val) {return false;}
+   while (hi - lo !== 1) 
+   {  // You should change this with a proper condition
+      // You will need to add things here
+      mid = Math.floor((lo + hi) / 2);
+      if (arr[mid] === val) {return true;}
+      (arr[mid] < val) ? lo = mid : hi = mid;
+   }
+   if (arr[lo] === val || arr[hi] === val) {return true;}
+   return false;
+   // You may need to add things here
 };
 
 /*
  * COUNTING TAGS
  */
 var countTags = function countTags(items) {
-   // Declare your local variables here. One was done for you.
-   var tagCounts;
-
-   // Add your code here
-
-
+   var tagCounts = {}; 
+   for (var i = 0; i < items.length; i++)
+   {
+      if (items[i]["tags"] === undefined) {continue;}
+      if (!Array.isArray(items[i]["tags"])) {continue;}
+      for (var d = 0; d < items[i]["tags"].length; d++)
+      {
+         if (/\S/.test(items[i]["tags"][d]) && tagCounts[items[i]["tags"][d]] === undefined) {tagCounts[items[i]["tags"][d]] = 1;}
+         else if (!/\S/.test(items[i]["tags"][d])) {continue;}
+         else {tagCounts[items[i]["tags"][d]] +=1;}
+      }
+      
+   }
    return tagCounts;
 };
 
@@ -35,5 +51,30 @@ var countTags = function countTags(items) {
  * EXTRACT HASHTAGS
  */
 var extractHashTags = function extractHashTags(str) {
-
+   var arr = [], t;
+   if (str.length === 0) {return arr;}
+   t = str.indexOf("#");
+   while(t != -1){
+      var em = "";
+      if (/^[a-zA-Z]+$/.test(str[t + 1])) {
+         for(var d = t + 1; d < str.length; d++)
+            {
+               if (!/^[a-zA-Z]+$/.test(str[d]))
+                  {
+                     break;
+                  }
+               em = em + str[d];   
+            }
+         arr.push(em);
+      }
+      t = str.indexOf("#", t + 1);
+   }
+   for (var i = 0; i < arr.length; i++)
+   {
+      for (var j = 0; j < arr.length; j++)
+      {
+         if (arr[j] === arr[i] && i !== j) {arr.splice(j,1);}
+      }
+   }
+   return arr;
 };
