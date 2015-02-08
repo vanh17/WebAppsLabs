1a0915a466c4645720aa0531cc8ba2c5ffd92058
vanh17 <vanh17@hanover.edu>
2015-02-09 03:16:04 +0700

    Failing tests for Stack methods (#5)

--- tests.spec.js
+++ tests.spec.js
@@ -39,4 +39,26 @@ describe( 'Stack methods:', function() {
       stack.push(2);
       expect(stack.isEmpty()).to.equal(false);
    });
+   it( 'push return a stack itself', function() {
+      expect(stack.push()).to.equal(stack);
+   });
+   it( 'pop returns error in an empty stack', function() {
+      expect(function() {stack.pop();}).to.throw(Error);
+   });
+   it( 'pop returns not error in an non-empty stack', function() {
+      stack.push(2);
+      expect(function() {stack.pop();}).to.not.throw(Error);
+   });
+   it( 'pop called after push method returns a pushed element', function() {
+      var v = Math.random();
+      stack.push(v);
+      expect(stack.pop()).to.equal(v);
+   });
+   it( 'consecutive pop returns elements in reverse order to push method', function() {
+      var v1 = Math.random(), v2 = Math.random() + 5;
+      stack.push(v1);
+      stack.push(v2);
+      expect(stack.pop()).to.equal(v2);
+      expect(stack.pop()).to.equal(v1);
+   })
 });
